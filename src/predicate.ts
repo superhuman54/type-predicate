f29ab7b4d44f2e1d290eb4b17f3f15af578c4331
Kihwan Kim <superhuman54@gmail.com>
2019-07-16 11:54:47 +0900

    :rocket: nuclear launch decteted

---
+++ src/predicate.ts
@@ -0,0 +1,34 @@
+
+
+export abstract class Predicate<T> {
+
+    abstract matches(a: T): boolean;
+
+    public and(other: Predicate<T>): Predicate<T> {
+        const _ = this;
+        return new class extends Predicate<T> {
+            matches(a: T): boolean {
+                return _.matches(a) && other.matches(a);
+            }
+        }
+    }
+
+    public or(other: Predicate<T>): Predicate<T> {
+        const _ = this;
+        return new class extends Predicate<T> {
+            matches(a: T): boolean {
+                return _.matches(a) || other.matches(a);
+            }
+        }
+    }
+
+    public negate(): Predicate<T> {
+        const _ = this;
+        return new class extends Predicate<T> {
+            matches(a: T): boolean {
+                return !_.matches(a);
+            }
+        }
+    }
+
+}
